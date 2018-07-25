4b0c7dc793eb5a46a6fb84660b164bd1e31cd316
Goodkov Vasiliy Aleksandrovich <qwegood@mail.ru>
2018-07-25 23:33:01 +0400

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -309,6 +309,18 @@ namespace {
         if (pos.blockers_for_king(Us) & s)
             b &= LineBB[pos.square<KING>(Us)][s];
 
+        if (Pt == QUEEN && b & pos.pieces(Us, ROOK) & PseudoAttacks[ROOK][s]
+                          | b & pos.pieces(Us, BISHOP) & PseudoAttacks[BISHOP][s])
+        {
+            bb = attacks_bb<  ROOK>(s, pos.pieces() ^ pos.pieces(Us, ROOK))
+               | attacks_bb<BISHOP>(s, pos.pieces() ^ pos.pieces(Us,BISHOP));
+               
+            if (pos.blockers_for_king(Us) & s)
+                bb &= LineBB[pos.square<KING>(Us)][s];
+               
+            attackedBy2[Us] |= attackedBy[Us][ALL_PIECES] & bb;
+        }
+        else
         attackedBy2[Us] |= attackedBy[Us][ALL_PIECES] & b;
         attackedBy[Us][Pt] |= b;
         attackedBy[Us][ALL_PIECES] |= b;
