54ff0222f089e4242b4573ff41c4bc19c41cdfbd
Goodkov Vasiliy Aleksandrovich <qwegood@mail.ru>
2018-07-28 16:15:59 +0400

    Update search.cpp

--- src/search.cpp
+++ src/search.cpp
@@ -622,7 +622,7 @@ namespace {
             if (ttValue >= beta)
             {
                 if (!pos.capture_or_promotion(ttMove))
-                    update_quiet_stats(pos, ss, ttMove, nullptr, 0, stat_bonus(depth));
+                    update_quiet_stats(pos, ss, ttMove, nullptr, 0, stat_bonus(depth - ONE_PLY));
 
                 // Extra penalty for a quiet TT move in previous ply when it gets refuted
                 if ((ss-1)->moveCount == 1 && !pos.captured_piece())
